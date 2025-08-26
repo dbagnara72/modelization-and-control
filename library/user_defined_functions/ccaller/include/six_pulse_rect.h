89f327c23640298b968a01865df7cbd8f9dcfdf6
dbagn <dbagnara@gmail.com>
2025-08-26 16:36:30 +0200

    no message

---
+++ library/user_defined_functions/ccaller/include/six_pulse_rect.h
@@ -0,0 +1,37 @@
+/* six pulses rectifier */
+
+#define MATH_PI_6		0.523598775598299
+#define MATH_PI_3		1.0471975511966
+#define MATH_PI_2		1.5707963267949
+#define MATH_PI			3.14159265358979
+#define MATH_2PI		6.28318530717959
+#define MATH_1_2		0.5
+#define MATH_SQRT3_2	0.866025403784439
+#define MATH_RAD_DEG	57.2957795130823
+#define PWIDTH			110
+
+
+typedef struct sprc_s {
+    float ramp_1; float ramp_2; float ramp_3; 
+	float ramp_4; float ramp_5; float ramp_6;  
+	
+	int synch_yA1; int synch_yA2; int synch_yA3; 
+	int synch_yA4; int synch_yA5; int synch_yA6;	
+	int synch_yB1; int synch_yB2; int synch_yB3; 
+	int synch_yB4; int synch_yB5; int synch_yB6;
+	int synch_y1; int synch_y2; int synch_y3;
+	int synch_y4; int synch_y5; int synch_y6;
+} sprc_t;
+
+typedef struct spr_p_s {
+	int p1;
+	int p2;
+	int p3;
+	int p4;
+	int p5;
+	int p6;
+} twvpr_p_t;
+
+float sprcProcess(sprc_t *spr, const float wt, const float alpha, const int block, twvpr_p_t *p);
+
+void sprcProcessSimulink(const float wt, const float alpha, const int block, twvpr_p_t *p);
