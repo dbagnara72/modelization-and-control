11d9d9f0057c9c0e8f8164dcf8b9f48dbb013fae
dab <dbagnara@gmail.com>
2025-09-07 09:07:39 +0200

    analysis of msmsm chain with state feedback

---
+++ foundation_of_mechanics/flexible_shaft/init_msmsm.m
@@ -0,0 +1,55 @@
+clear all
+close all
+clc
+
+options = bodeoptions;
+options.FreqUnits = 'Hz';
+
+J1 = 0.025;
+J2 = J1/sqrt(3);
+J3 = J1;
+b1 = 0.01;
+b2 = 0.01;
+b3 = 0.01;
+b_theta = 0;
+k_theta = 1;
+
+f0_msm = 1/2/pi*sqrt(k_theta*(J1+J2)/(J1*J2))
+
+f0_msmsm = 1/2/pi/sqrt(2)/J1/J2/J3*sqrt(-J1^2*J2^2*J3*k_theta - 2*J1^2*J2*J3^2*k_theta ...
+    -J1*J2^2*J3^2*k_theta - J1*J2*J3*sqrt(J1^2*J2^2-2*J1*J2^2*J3+4*J1^2*J3^2+J2^2*J3^2)*k_theta)
+
+f1_msmsm = 1/2/pi/sqrt(2)/J1/J2/J3*sqrt(-J1^2*J2^2*J3*k_theta - 2*J1^2*J2*J3^2*k_theta ...
+    -J1*J2^2*J3^2*k_theta + J1*J2*J3*sqrt(J1^2*J2^2-2*J1*J2^2*J3+4*J1^2*J3^2+J2^2*J3^2)*k_theta)
+
+Ts = 1/1e3;
+Tcontrol=Ts;
+Tc = Ts;
+simlength = 30;
+Nc=floor(simlength/Tc);
+load_torque = 10;
+
+s=tf('s');
+z=tf('z',Ts);
+
+A = [-(b1+b_theta)/J1 b_theta/J1 0 -1/J1 0; ...
+    b_theta/J2 -(b2+b_theta)/J2 b_theta/J2 1/J2 -1/J2;...
+    0 b_theta/J3 -(b3+b_theta)/J3 0 1/J3;...
+    k_theta -k_theta 0 0 0; ...
+    0 k_theta -k_theta 0 0];
+B = [1/J1 0 0 0 0]';
+E = [0 0 1/J2 0 0]';
+C = [1 0 0 0 0];
+
+%% sf
+Q=[Ts 0 0 0 0; 0 Ts 0 0 0; 0 0 Ts 0 0; ...
+    0 0 0 1 0; 0 0 0 0 1];
+R=Ts;
+K = lqr(A,B,Q,R)
+
+%% ISF + SOBS control design in d.t.d.
+
+
+
+
+
--- foundation_of_mechanics/flexible_shaft/plotting_step_responce_sf.m
+++ foundation_of_mechanics/flexible_shaft/plotting_step_responce_sf.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 
-load("sim_results_1.mat");
+load("sim_results_2.mat");
 
 tratto1 = 3;
 tratto2 = 3;
@@ -11,7 +11,7 @@ colore1 = [0.25 0.25 0.25];
 colore2 = [0.5 0.5 0.5];
 colore3 = [0.75 0.75 0.75];
 t1c = time_tc_msmsm_sim(end) - Nc*Tc;
-t2c = t1c + 4;
+t2c = t1c + 1;
 
 fontsize_plotting = 14;
 
@@ -23,12 +23,12 @@ plot(time_tc_msmsm_sim,omega_2_msmsm_sim,'-','LineWidth',tratto1,'Color',colore2
 hold on
 plot(time_tc_msmsm_sim,omega_3_msmsm_sim,'-','LineWidth',tratto1,'Color',colore3);
 hold off
-title('System pulse response','Interpreter','latex','FontSize',fontsize_plotting);
+title('System pulse response - inner speeds','Interpreter','latex','FontSize',fontsize_plotting);
 legend('$\omega_1$','$\omega_2$','$\omega_3$','Location','best',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
 ylabel('$p.u.$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'ylim',[-0.5 2.25]);
+set(gca,'ylim',[-0.5 0.5]);
 set(gca,'xlim',[t1c t2c]);
 grid on
 subplot 212
@@ -38,7 +38,7 @@ plot(time_tc_msmsm_sim,tau_theta_23_msmsm_sim,'-','LineWidth',tratto1,'Color',co
 hold on
 plot(time_tc_msmsm_sim,tau_m_msmsm_sim,'-','LineWidth',tratto1,'Color',colore3);
 hold off
-title('System pulse response','Interpreter','latex','FontSize',fontsize_plotting);
+title('System pulse response - inner torques','Interpreter','latex','FontSize',fontsize_plotting);
 legend('$\tau_{12}$','$\tau_{23}$','$\tau_m$','Location','best',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
@@ -46,5 +46,5 @@ ylabel('$p.u.$','Interpreter','latex','FontSize', fontsize_plotting);
 set(gca,'ylim',[-0.5 1.25]);
 set(gca,'xlim',[t1c t2c]);
 grid on
-print('plant_msmsm_pulse_responce','-djpeg');
+print('plant_msmsm_pulse_responce_sf','-djpeg');
 
